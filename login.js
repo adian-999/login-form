099ad5277fd21eb067ff136ec37056339335353a
adian-999 <adianahmmed999@gmail.com>
2024-09-19 01:37:06 +0600

    home added

--- login.js
+++ login.js
@@ -1,7 +1,14 @@
 document.getElementById('button-login').addEventListener('click', function(event) {
     event.preventDefault();
-    console.log('login button clicked');
+    
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    if(phoneNumber==='5' && pinNumber==='1234'){
+        console.log('You are logged in');
+        window.location.href = 'home.html'
+    }
+    else{
+        alert('Please enter correct number and password')
+    }
 });
 
