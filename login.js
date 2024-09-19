d31e146b6069fa16d99524eb6fe5a344e7ccaec2
adian-999 <adianahmmed999@gmail.com>
2024-09-19 17:20:43 +0600

    num changed

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ document.getElementById('button-login').addEventListener('click', function(event
     
     const phoneNumber = document.getElementById('phone-number').value;
     const pinNumber = document.getElementById('pin-number').value;
-    if(phoneNumber==='5' && pinNumber==='1234'){
+    if(phoneNumber==='01700539292' && pinNumber==='1234'){
         console.log('You are logged in');
         window.location.href = 'home.html';
     }
